a75ad0bdd484dcab02f22c851729341c9010b29e
Eli7273 <eliasafgonzalez0@gmail.com>
2026-02-02 12:39:30 -0800

    changes

--- obj/Release/net9.0/RPSLS.AssemblyInfo.cs
+++ obj/Release/net9.0/RPSLS.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RPSLS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3212b5e3fc3b99b2c09b52a1c0ccd4a53dca40b5")]
 [assembly: System.Reflection.AssemblyProductAttribute("RPSLS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RPSLS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
